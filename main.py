874a9526dc91bd8c3d6ca4980512e34b82407d17
Abhishek Ambastha <aambasth@cisco.com>
2016-05-22 21:26:46 -0700

    Modified to fit faster-rcnn

--- main.py
+++ main.py
@@ -8,44 +8,42 @@ import json
 def process_seqs():
     """Convert the sequence files and save to similar dir structure"""
 
+    if not os.path.exists('target/images'):
+        os.mkdir('target/images')
+
     for dir_name in glob.glob('data/set*'):
         parent_dir = os.path.split(dir_name)[-1]
-        if not os.path.exists('target/{}'.format(parent_dir)):
-            os.mkdir('target/{}'.format(parent_dir))
         for seq_path in glob.glob('{}/*.seq'.format(dir_name)):
-            #vid = cv2.VideoCapture(seq_path)
             current_dir = os.path.splitext(os.path.basename(seq_path))[0]
-            if not os.path.exists('target/{}/{}'.format(parent_dir, current_dir)):
-                os.mkdir('target/{}/{}'.format(parent_dir, current_dir))
-            #save it here!
             print('Converting {}/{}.seq'.format(parent_dir, current_dir))
-            #convert_sequence(vid, 'target/{}/{}'.format(parent_dir, current_dir))
-            read_sequence(seq_path, 'target/{}/{}'.format(parent_dir, current_dir))
+            read_sequence(seq_path, 'target/images', parent_dir + '_' + current_dir)
 
 
 def process_annotations():
     """Convert annotations to json file format"""
 
+    final_annotation = {}
+    if not os.path.exists('target/Annotations'):
+        os.mkdir('target/Annotations')
+
     for dir_name in glob.glob('data/annotations/set*'):
         parent_dir = os.path.split(dir_name)[-1]
-        if not os.path.exists('target/{}'.format(parent_dir)):
-            os.mkdir('target/{}'.format(parent_dir))
         for vbb_file in glob.glob('{}/*.vbb'.format(dir_name)):
             current_dir = os.path.splitext(os.path.basename(vbb_file))[0]
             vbb = loadmat(vbb_file)
             print('Converted annotations from {}'.format(vbb_file))
             annotation = convert_annotations(vbb)
+            final_annotation.update(annotation)
 
-            if not os.path.exists('target/{}/{}'.format(parent_dir, current_dir)):
-                os.mkdir('target/{}/{}'.format(parent_dir, current_dir))
-            with open('target/{}/{}/annotations.json'.format(parent_dir, current_dir), 'w') as f:
-                json.dump(annotation, f)
+    with open('target/Annotations/annotations.json', 'w') as f:
+        json.dump(final_annotation, f)
+
 
 def main():
     if not os.path.exists('target'):
         os.mkdir('target')
 
-    process_seqs()
+    #process_seqs()
 
     process_annotations()
 
